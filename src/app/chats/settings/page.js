400137647f05058c9e88ae385f911e71efaff252
faizan-shaikh-dev <faizangulab4276@gmail.com>
2026-01-12 15:30:41 +0530

    build setting panel

--- src/app/chats/settings/page.js
+++ src/app/chats/settings/page.js
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client";
+
+import Accounts from "@/components/settings/Accounts";
+import Chats from "@/components/settings/Chats";
+import Notifications from "@/components/settings/Notifications";
+import Privacy from "@/components/settings/Privacy";
+import Storage from "@/components/settings/Storage";
+import { useSearchParams } from "next/navigation";
 
 const Page = () => {
-  return (
-    <div>This is Setting page</div>
-  )
-}
+  const searchParams = useSearchParams();
 
-export default Page
+  const option = searchParams.get("option");
+
+  console.log(option);
+
+  if (option === "accounts") {
+    return <Accounts />;
+  } else if (option === "chats") {
+    return <Chats />;
+  } else if (option === "notifications") {
+    return <Notifications />;
+  } else if (option === "privacy") {
+    return <Privacy />;
+  } else if (option === "store") {
+    return <Storage />;
+  } else {
+    return <div>This is the setting page</div>;
+  }
+  return;
+};
+
+export default Page;
